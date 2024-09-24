e6980df5d880b1e2e09c6c2ccaf74a2e1f1fef62
DaWiiX <DaWiiX_CHD@outlook.com>
2024-09-24 09:28:42 +0800

    UPDATE encoder layer

---
+++ include/op/encoder.h
@@ -0,0 +1,32 @@
+#ifndef INCLUDE_OP_ENCODE_H_
+#define INCLUDE_OP_ENCODE_H_
+
+#include <sentencepiece_processor.h>
+#include "layer.h"
+
+namespace op
+{
+    class EncodeLayer : public Layer
+    {
+    public:
+        explicit EncodeLayer(base::DeviceType device_type);
+
+        explicit EncodeLayer(
+            base::DeviceType device_type, bool has_bos, bool has_eos,
+            std::unique_ptr<sentencepiece::SentencePieceProcessor> sentence_piece_processor);
+
+        std::vector<int32_t> encode(const std::string &sentence) const;
+
+        std::string decode(int32_t token_id) const;
+
+        std::string decode(const std::vector<int32_t> &token_ids) const;
+
+        int32_t eos() const;
+
+    private:
+        bool has_bos_ = true;
+        bool has_eos_ = false;
+        std::unique_ptr<sentencepiece::SentencePieceProcessor> spe;
+    };
+} // namespace op
+#endif // INCLUDE_OP_ENCODE_H_
