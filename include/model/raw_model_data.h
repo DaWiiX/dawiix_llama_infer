f95faf85f84b564e0f6dcb303e524eb380180707
DaWiiX <DaWiiX_CHD@outlook.com>
2024-09-24 13:38:18 +0800

    Update llama2 model

---
+++ include/model/raw_model_data.h
@@ -0,0 +1,31 @@
+#ifndef INCLUDE_MODEL_RAW_MODEL_DATA_H_
+#define INCLUDE_MODEL_RAW_MODEL_DATA_H_
+
+#include <cstddef>
+#include <cstdint>
+namespace model
+{
+    struct RawModelData
+    {
+        ~RawModelData();
+        int32_t fd = -1;
+        size_t file_size = 0;
+        void *data = nullptr;
+        void *weight_data = nullptr;
+
+        virtual const void *weight(size_t offset) const = 0;
+    };
+
+    struct RawModelDataFp32 : RawModelData
+    {
+        const void *weight(size_t offset) const override;
+    };
+
+    struct RawModelDataInt8 : RawModelData
+    {
+        const void *weight(size_t offset) const override;
+    };
+
+} // namespace model
+
+#endif // INCLUDE_MODEL_RAW_MODEL_DATA_H_
